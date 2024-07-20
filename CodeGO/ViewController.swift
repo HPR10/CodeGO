fa3c6f6b1d0badcfab3bdf9a0a77e3b278ae1186
Hugo Pinheiro <hugo_hp10@hotmail.com>
2024-07-19 21:35:49 -0300

    Set up initial UI with ViewCode and configure constraints

--- CodeGO/ViewController.swift
+++ CodeGO/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Criação do UILabel com configuração inicial
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello ViewCode"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // Chamado após a view do controlador ter sido carregada na memória e apenas uma vez
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        // Configuração das views e constraints
+        setupViews()
+        setupConstraints()
     }
-
-
+    
+    // Adiciona subviews à hierarquia de visualização
+    private func setupViews() {
+        view.addSubview(label)
+    }
+    
+    // Configura as constraints usando NSLayoutConstraint
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
+
 
